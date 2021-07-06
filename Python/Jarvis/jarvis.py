aa114393bb44bd08afd2e068e42f99d49c75dd29
Alonso García <globulitorojo5@gmail.com>
2021-07-06 12:58:00 -0500

    Agrego reconocimiento de voz Nota*No funciona, toca corregir proximamente*

--- Python/Jarvis/jarvis.py
+++ Python/Jarvis/jarvis.py
@@ -1,6 +1,7 @@
 #Importamos la libreria
 import pyttsx3
 import datetime
+import speech_recognition as sr
 
 #Inicializamos pyttsx3
 engine = pyttsx3.init()
@@ -10,20 +11,21 @@ voice = engine.getProperty("voices")
 engine.setProperty("voices", voice[0].id)
 
 def speak(audio = str()):
-    """Esta funcion hace que el asistente hable lo que se le pase por la variable de audio
+    """Hace que el asistente hable lo que se le pase por la variable de audio
     Audio: String"""
     engine.say(audio)
     engine.runAndWait()
 
 def time() -> str():
-    """Esta funcion dice la hora actual"""
+    """Dice la hora actual"""
     return f"La hora es {datetime.datetime.now().strftime('%I:%M:%S')}"
 
 def date() -> str():
-    """Esta funcion regresa la fecha actual"""
+    """Regresa la fecha actual"""
     return f"Hoy es {int(datetime.datetime.now().year)} del mes {int(datetime.datetime.now().month)} del día {int(datetime.datetime.now().day)}"
 
 def sayGood():
+    """Nos dice los buenos días/tardes/noces"""
     hour = datetime.datetime.now().hour
     if hour >=6 and hour < 12:
         speak("Buenos días")
@@ -32,13 +34,30 @@ def sayGood():
     else:
         speak("Buenas noches")
     
-
 def wishme():
-    """Esta fucion da la bienvenida"""
+    """Da la bienvenida"""
+    sayGood()
     speak("¡Bienvenido de vuelta!")
     speak(time())
-    sayGood()
     speak(date())
     speak("¿En que puedo ayudarte?")
 
-wishme()
+def takeCommand():
+    """Reconoce la voz"""
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Escuchando...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Reconociendo")
+        query = r.recognize_google(audio, "es=MX")
+        print(query)
+    except Exception as e:
+        print(e)
+        speak("Repíte lo que dijiste, porfavor")
+        return "None"
+    return query
+
+takeCommand()
