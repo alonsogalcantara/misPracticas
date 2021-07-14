81867b18d4c747d7a4cc9d5acab80b82a59cbd6e
Alonso García <globulitorojo5@gmail.com>
2021-07-14 01:13:42 -0500

    Añado prettier al auto save y verifico que funcione :D

--- Python/Pruebas/busqueda_binaria.py
+++ Python/Pruebas/busqueda_binaria.py
@@ -1,6 +1,7 @@
 import random
 
-def burbuja(a = list()):
+
+def burbuja(a=list()):
     '''Ordena de menor a mayor una lista dada
     Regresa la misma lista ya ordenada'''
     for i in range(1, len(a)):
@@ -11,26 +12,29 @@ def burbuja(a = list()):
                 a[j+1] = aux
     return a
 
+
 def busqueda_binaria(lista=list(), comienzo=0, final=0, objetivo=0):
-    if comienzo > final:#Evita que la recursion sea infitina
+    if comienzo > final:  # Evita que la recursion sea infitina
         return False
     elif comienzo == final:
         return False
 
     print(f'Buscando {objetivo} entre {lista[comienzo]} y {lista[final-1]}')
 
-    medio = (comienzo+final) // 2 #Obtiene la mitad de la lista
+    medio = (comienzo+final) // 2  # Obtiene la mitad de la lista
 
-    if lista[medio] == objetivo: #Compara si el objetivo de la busqueda es igual a la mitad
+    if lista[medio] == objetivo:  # Compara si el objetivo de la busqueda es igual a la mitad
         return True
-    elif lista[medio] < objetivo:#Compara si el objetivo de la busqueda es menor a la mitad
+    elif lista[medio] < objetivo:  # Compara si el objetivo de la busqueda es menor a la mitad
         return busqueda_binaria(lista=lista, comienzo=medio + 1, final=final, objetivo=objetivo)
-    else: # Compara si el objetivo de la busqueda es mayor a la mitad / si no son los otras dos opciones
+    else:  # Compara si el objetivo de la busqueda es mayor a la mitad / si no son los otras dos opciones
         return busqueda_binaria(lista=lista, comienzo=comienzo, final=medio - 1, objetivo=objetivo)
     pass
 
+
 if __name__ == '__main__':
     a = [random.randint(0, 10) for i in range(0, 10)]
-    objetivo = random.randint(0,10)
+    objetivo = random.randint(0, 10)
     print(f'Buscando {objetivo} en la lista {burbuja(a)}')
-    print('Se encontro el objetivo' if busqueda_binaria(lista=burbuja(a), comienzo=0, final=len(burbuja(a)), objetivo=objetivo) else 'No se encontro el objetivo')
+    print('Se encontro el objetivo' if busqueda_binaria(lista=burbuja(a), comienzo=0,
+          final=len(burbuja(a)), objetivo=objetivo) else 'No se encontro el objetivo')
---
+++ Python/Pruebas/pruebaTipeo.py
@@ -0,0 +1,18 @@
+a = 10
+b = 1
+c = 3
+
+if a == 10:
+    print("Hola")
+
+if b != 5:
+    print("Hola 2")
+
+if a >= 3:
+    print("efbd")
+
+if c <= a:
+    print("No*3")
+
+    for v in range(10):
+        print(1*v)
