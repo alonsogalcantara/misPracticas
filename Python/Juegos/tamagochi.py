b10567bda86af1e0c348e1f755d55cc67248fd7f
Alonso García <globulitorojo5@gmail.com>
2023-05-18 17:53:43 -0600

    Agrego nuevo flujo para otro intento

--- Python/Juegos/tamagochi.py
+++ Python/Juegos/tamagochi.py
@@ -1,4 +1,7 @@
 import random
+import time
+import os
+
 
 class Tamagotchi:
     def __init__(self):
@@ -32,7 +35,7 @@ class Tamagotchi:
     ~~   ~~
     ~~   ~~
     ~~   ~~'''
-        
+
         self.play_frame = '''
         (__)
         (oo)
@@ -42,20 +45,18 @@ class Tamagotchi:
     ~~   ~~  / '\
     ~~   ~~
     ~~   ~~'''
-        
 
     def __str__(self):
         '''
         return: status del animalito
         '''
-
         if self.hunger > 50:
-            print(f"{self.name} tiene hambre")
+            print(f"- {self.name} tiene hambre")
 
-        if self.happiness > 40 and self.happiness < 70:
-            print(f"{self.name} esta molesto")
+        if self.happiness > 45:
+            print(f"- {self.name} esta molesto")
 
-        return f"\n{self.name} is {self.age} years old. Hunger: {self.hunger}, Happiness: {self.happiness}, Health: {self.health}"
+        return f"{self.name} is {self.age} years old. Hunger: {self.hunger}, Happiness: {self.happiness}, Health: {self.health}"
 
     def feed(self):
         '''
@@ -81,11 +82,11 @@ class Tamagotchi:
         '''
         Duerme al animalito
         hunger: incrementa entre 5 y 8pts
-        happiness: incrementa entre 4 y 9
+        happiness: incrementa entre 4 y 6
         hralth: decrementa entre 5 y 10
         '''
         self.hunger += random.randint(5, 8)
-        self.happiness += random.randint(4, 9)
+        self.happiness += random.randint(4, 6)
         self.health -= random.randint(5, 10)
         print(self.sleep_frame)
 
@@ -94,11 +95,15 @@ class Tamagotchi:
         Marca el tiempo por cada decision
         hunger: aumenta entre 1 y 10
         happiness: decrementa entre 1 y 5
-        health: decrementa entre 1 y 7
+        health: decrementa entre 1 y 4, sin embargo si los stats de hunger o happiness estan altos su valor incrementa de entre 5 y 9pts
         '''
         self.hunger += random.randint(1, 10)
         self.happiness -= random.randint(1, 5)
-        self.health -= random.randint(5, 10)
+
+        if self.hunger > 80 or self.happiness > 78:
+            self.health -= random.randint(5, 9)
+        else:
+            self.health -= random.randint(1, 4)
 
         # Si la salud es mayor a 100 esta no aumentara mas.
         if self.health > 100:
@@ -107,39 +112,48 @@ class Tamagotchi:
     def is_alive(self):
         return self.health > 0
 
+
 def main():
-    # Create a new Tamagotchi
-    tamagotchi = Tamagotchi()
+    while True:
+        # Create a new Tamagotchi
+        tamagotchi = Tamagotchi()
 
-    # Give the Tamagotchi a name
-    tamagotchi.name = input("What is your Tamagotchi's name? ")
+        # Give the Tamagotchi a name
+        tamagotchi.name = input("What is your Tamagotchi's name? ")
 
-    # Play with the Tamagotchi
-    while tamagotchi.is_alive():
-        print(tamagotchi)
+        # Play with the Tamagotchi
+        while tamagotchi.is_alive():
+            print(tamagotchi)
+
+            list_options = ['feed', 'play', 'sleep']
+
+            action = random.choice(list_options)
+
+            print(f'\nElegiste: {action}')
+            if action == "feed":
+                tamagotchi.feed()
+            elif action == "play":
+                tamagotchi.play()
+            elif action == "sleep":
+                tamagotchi.sleep()
+            elif action == "quit":
+                break
+
+            # Let the Tamagotchi age
+            tamagotchi.age += 1
+            tamagotchi.tick()
+
+            time.sleep(1)
+            os.system("cls")
+
+        if ~tamagotchi.is_alive():
+            print(f"{tamagotchi.name} dies at the age of {tamagotchi.age} years")
         
-        list_options = ['feed', 'play', 'sleep']
-        
-        action = random.choice(list_options)
-
-        print(f'Elegiste: {action}')
-        if action == "feed":
-            tamagotchi.feed()
-        elif action == "play":
-            tamagotchi.play()
-        elif action == "sleep":
-            tamagotchi.sleep()
-        elif action == "quit":
+        if ('n' in input('Volver a intentar?: S/n\n').lower()):
             break
+        
 
-        # Let the Tamagotchi age
-        tamagotchi.age += 1
-        tamagotchi.tick()
 
-    if tamagotchi.is_alive():
-        print("Your Tamagotchi is alive!")
-    else:
-        print("Your Tamagotchi died.")
 
 if __name__ == "__main__":
     main()
